Razor-Developers/android_packages_apps_Settings

Settings : add build date in About Phone

src/com/android/settings/DeviceInfoSettings.java
@@ -73,6 +73,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_BUILD_NUMBER = "build_number";
     private static final String KEY_DEVICE_MODEL = "device_model";
     private static final String KEY_RAZOR_VERSION = "razor_version";
+    private static final String KEY_RAZOR_BUILD_DATE = "build_date";
     private static final String KEY_SELINUX_STATUS = "selinux_status";
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
@@ -127,6 +128,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
         setValueSummary(KEY_RAZOR_VERSION, "ro.razor.version");
+        setValueSummary(KEY_RAZOR_BUILD_DATE, "ro.build.date");
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());

Intense-M/packages_apps_Settings

Signed-off-by: vibhu0009 <cvibhu0009@gmail.com>
Conflicts:
	res/values/razor_strings.xml
	src/com/android/settings/DeviceInfoSettings.java

src/com/android/settings/DeviceInfoSettings.java
@@ -70,6 +70,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_BUILD_NUMBER = "build_number";
     private static final String KEY_DEVICE_MODEL = "device_model";
     private static final String KEY_INTENSE_VERSION = "intense_version";
+    private static final String KEY_INTENSE_BUILD_DATE = "build_date";
     private static final String KEY_SELINUX_STATUS = "selinux_status";
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
@@ -122,6 +123,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
         setValueSummary(KEY_INTENSE_VERSION, "ro.intense.version");
+        setValueSummary(KEY_INTENSE_BUILD_DATE, "ro.build.date");
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());