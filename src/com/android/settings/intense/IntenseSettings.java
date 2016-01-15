e608f9f420d5998c544751b5ba3c25b2b9e88d09
vibhu0009 <cvibhu0009@gmail.com>
2016-01-15 11:25:24 +0100

    Settings : Intense Tweaks
    Signed-off-by: vibhu0009 <cvibhu0009@gmail.com>

---
+++ src/com/android/settings/intense/IntenseSettings.java
@@ -0,0 +1,23 @@
+
+package com.android.settings.intense;
+
+import android.os.Bundle;
+
+import com.android.settings.InstrumentedFragment;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class IntenseSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.intense_tweaks);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return InstrumentedFragment.INTENSESETTINGS;
+    }
+ }
